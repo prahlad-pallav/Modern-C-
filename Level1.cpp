088ad5103917da0a351eff8b3f11b482e8c26f82
Prahlad Pallav <135969574+prahlad-pallav@users.noreply.github.com>
2024-01-02 13:15:15 +0530

    Create Level1.cpp

---
+++ Level1.cpp
@@ -0,0 +1,15 @@
+// Contains the facilities for I/O
+#include <iostream>
+
+// Open the std namespace
+using namespace std;
+
+// Entry point of the program
+int main() {
+
+	//Print the string on the screen
+	cout << "Hello World" << endl;
+
+	// Return the exit code
+	return 0;
+}
