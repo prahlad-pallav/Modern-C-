239f65cfab86b40042f3ba16a1602e9b053940cc
Prahlad Pallav <135969574+prahlad-pallav@users.noreply.github.com>
2024-01-02 13:36:59 +0530

    Create Level2.cpp

---
+++ Level2.cpp
@@ -0,0 +1,35 @@
+// Contains the facilities for I/O
+#include <iostream>
+
+// Open the std namespace
+using namespace std;
+
+// Entry point of the program
+int main() {
+
+	// Scalar types
+	int i = 10;
+	cout << "Value of i is " << i << endl;
+
+	char ch = 'a';
+	cout << "Value of ch is " << ch << endl;
+
+	bool flag = false;
+	cout << "Value of flag is " << flag << endl;
+
+	float f = 1.23f;
+	cout << "Value of f is " << f << endl;
+
+	double d = 234.433;
+	cout << "Value of d is " << d << endl;
+
+
+	// Vector types
+	int arr[5];
+
+	int arr1[5] = { 1, 2, 4, 5, 6 };
+
+
+	// Return the exit code
+	return 0;
+}
