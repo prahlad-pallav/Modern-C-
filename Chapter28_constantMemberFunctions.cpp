635b679be25ac64016b624395cf1c3326dfa1738
Prahlad Pallav <135969574+prahlad-pallav@users.noreply.github.com>
2024-01-11 17:21:52 +0530

    Create Chapter28_constantMemberFunctions.cpp

---
+++ Chapter28_constantMemberFunctions.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+private:
+    // Non-static data member initializer
+    float fuel = 0;
+    float speed = 0;
+    int passengers = 0;
+    static inline int totalCount = 0; // Using inline specifier
+
+public:
+
+    Car() : fuel(0), speed(0), passengers(0) {
+        ++totalCount;
+    }
+
+    Car(float amount) : fuel(amount), speed(0), passengers(0) {
+        ++totalCount;
+    }
+
+    ~Car() {
+        cout << "Car object is destroyed!" << endl;
+        --totalCount;
+    }
+
+public:
+    void FillFuel(float amount) {
+        this->fuel = amount;
+    }
+
+    void Accelerate() {
+        this->speed++;
+        this->fuel -= 0.5f;
+    }
+
+    void Brake() {
+        this->speed = 0;
+    }
+
+    void AddPassengers(int count) {
+        this->passengers = count;
+    }
+
+    void Dashboard() const {
+        cout << "Fuel: " << fuel << endl;
+        cout << "Speed: " << speed << endl;
+        cout << "Passengers: " << passengers << endl;
+        cout << "Total count: " << totalCount << endl;
+    }
+
+    static void ShowCount() {
+        cout << "Total count: " << totalCount << endl;
+    }
+};
+
+    int main() {
+        Car::ShowCount();
+        const Car car(4);
+        car.ShowCount();
+        car.Dashboard();
+
+        return 0;
+    }
